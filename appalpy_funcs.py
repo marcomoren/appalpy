b94f58b080000d556bab4b505aa45f2b26b9a316
Marco <morenomarco03@gmail.com>
2021-12-28 16:33:37 +0100

    modified functions to include more insuer-preferred parameters

--- appalpy_funcs.py
+++ appalpy_funcs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import pandas as pd
 from datetime import datetime
+import pandas as pd
 from dateutil.relativedelta import relativedelta
+from typing import Union
 
-def ymformat(date):
+def ymformat(date:datetime):
     """
     This function generates a string containing year and month to be used to access the relevant URL (e.g., "2021_07")
     """
@@ -12,7 +13,7 @@ def ymformat(date):
     return ymformat
 
 
-def periods(start, end):
+def periods(start:datetime, end:datetime):
     """
     This function generates a list of years and months between the starting and ending dates (inclusive) selected by the user.
     Such list is used to iterate through the relevant URLs.  
@@ -24,14 +25,16 @@ def periods(start, end):
         moving_period = moving_period + relativedelta(months=+1)
     return periods_list
 
-def get_tender_data(date):
+def get_tender_data(date:datetime):
     """
     This function opens the intended URL containing data on monthly tenders, opens the csv dataset contained inside the downloadable zip file 
     and generates a pandas DataFrame.
     """
     base_url = "https://dati.anticorruzione.it/opendata/download/dataset/cig-"
     full_url = base_url + ymformat(date)[:4] + "/filesystem/cig_csv_" + ymformat(date) + ".zip"
-    tender_csv_file = pd.read_csv(full_url, compression='zip', sep=';', warn_bad_lines=True, dtype='object')
+    #requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS='ALL'
+    #requests.packages.urllib3.contrib.pyopenssl.extract_from_urllib3()
+    tender_csv_file = pd.read_csv(full_url, compression='zip', sep=';', dtype='object')
     return tender_csv_file
 
 def get_awards_data():
@@ -40,7 +43,7 @@ def get_awards_data():
     generates a pandas DataFrame.
     """
     awards_url = "https://dati.anticorruzione.it/opendata/download/dataset/aggiudicazioni/filesystem/aggiudicazioni_csv_0.zip"
-    awards_csv_file = pd.read_csv(awards_url, compression='zip', sep=';', warn_bad_lines=True, dtype='object')
+    awards_csv_file = pd.read_csv(awards_url, compression='zip', sep=';', dtype='object')
     return awards_csv_file
 
 def get_contractor_data():
@@ -49,10 +52,10 @@ def get_contractor_data():
     and generates a pandas DataFrame.
     """
     contractor_url = "https://dati.anticorruzione.it/opendata/download/dataset/aggiudicatari/filesystem/aggiudicatari_csv_0.zip"
-    contractor_csv_file = pd.read_csv(contractor_url, compression='zip', sep=';', warn_bad_lines=True, dtype='object')
+    contractor_csv_file = pd.read_csv(contractor_url, compression='zip', sep=';', dtype='object')
     return contractor_csv_file
 
-def reduce_data(df, keywords, field):
+def reduce_data(df, keywords:Union[list, str], field:str):
     """
     This function reduces the dataset based on conditions provided by the user
     """
@@ -61,5 +64,5 @@ def reduce_data(df, keywords, field):
     elif type(keywords) is str:
         df = df[df[field].str.contains(keywords.lower(), case=False, na = False)]
     else:
-        raise ValueError("user input must be passed as a list or a string")
+        raise TypeError("user input must be passed as a list or a string")
     return df
